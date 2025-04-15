3c2613e6a24a242f24ad6545907e1f60d3d02f4b
Matias F <matiasfavorito@gmail.com>
2025-04-15 16:46:45 -0300

    uml ej9 patrones

--- proyectosMaven_OO2/ej8Patrones/src/main/java/objetos/dos/ej8Patrones/CRC32_Calculator.java
+++ proyectosMaven_OO2/ej8Patrones/src/main/java/objetos/dos/ej8Patrones/CRC32_Calculator.java
@@ -1,5 +1,12 @@
 package objetos.dos.ej8Patrones;
 
-public class CRC32_Calculator implements CRCCalculatorStrategy {
+import java.util.zip.CRC32;
 
+public class CRC32_Calculator implements CRCCalculatorStrategy {
+	
+	public long crcFor(String datos) {
+		CRC32 crc = new CRC32();
+		crc.update(datos.getBytes());
+		return crc.getValue();
+	}
 }
--- proyectosMaven_OO2/ej8Patrones/src/main/java/objetos/dos/ej8Patrones/CRCCalculatorStrategy.java
+++ proyectosMaven_OO2/ej8Patrones/src/main/java/objetos/dos/ej8Patrones/CRCCalculatorStrategy.java
@@ -1,5 +1,6 @@
 package objetos.dos.ej8Patrones;
 
 public interface CRCCalculatorStrategy {
-
+	
+	public long crcFor(String datos);
 }
