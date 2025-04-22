4cbf0f61b7f8a10c457724d0f55c209f1016e0ab
Matias F. <86623987+PichiCoder@users.noreply.github.com>
2025-04-22 19:05:39 -0300

    Update RC4Adapter.java

--- proyectosMaven_OO2/ej_10Patrones/src/main/java/objetos/dos/ej_10Patrones/RC4Adapter.java
+++ proyectosMaven_OO2/ej_10Patrones/src/main/java/objetos/dos/ej_10Patrones/RC4Adapter.java
@@ -2,6 +2,10 @@ package objetos.dos.ej_10Patrones;
 
 public class RC4Adapter extends MecanismoDeCifrado {
 
+
+	//faltaria el el constructor
+	// No darle mucha bola a "de donde sale la key". Se puede hacer un getKey() ponele y fue.
+	// Igualmente la clase que viene puedo preguntarle devuelta por las dudas.
 	@Override
 	public String encriptar(String msj) {
 		RC4 rc4 = new RC4();
@@ -16,6 +20,4 @@ public class RC4Adapter extends MecanismoDeCifrado {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-
 }
