012b905e416a7e0223c0183336de3355599759f8
Matias F. <86623987+PichiCoder@users.noreply.github.com>
2025-04-22 19:04:05 -0300

    Update Mensajero.java

--- proyectosMaven_OO2/ej_10Patrones/src/main/java/objetos/dos/ej_10Patrones/Mensajero.java
+++ proyectosMaven_OO2/ej_10Patrones/src/main/java/objetos/dos/ej_10Patrones/Mensajero.java
@@ -8,12 +8,12 @@ public class Mensajero {
 	// Duda, la llave se crea aca ?
 	public void enviar (String msj) {
 		mecanismo.encriptar(msj);
-		//...
+		//se envia msj...
 	}
 	
 	public void recibir (String msj) {
 		mecanismo.encriptar(msj);
-		//...
+		//se recibe y se guarda msj...
 	}
 
 	public void setMecanismo(MecanismoDeCifrado mecanismo) {
