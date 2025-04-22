c14543ad730eddb586806c83f722614a597def1d
Matias F. <86623987+PichiCoder@users.noreply.github.com>
2025-04-22 19:01:17 -0300

    Update Topografia.java

--- proyectosMaven_OO2/ej_11Patrones/src/main/java/objetos/dos/ej_11Patrones/Topografia.java
+++ proyectosMaven_OO2/ej_11Patrones/src/main/java/objetos/dos/ej_11Patrones/Topografia.java
@@ -11,6 +11,13 @@ public interface Topografia {
 	public boolean soyIgualATierra();
 	public boolean soyIgualAPantano();
 	public boolean soyIgualAMixta(Mixta m);
+
+	//tambien se podria con un metodo igual(tipo t) que reciba por parametro cada tipo disinto de topografia
+	/*
+	 * public boolean esIgual (Topografia t) {
+	 * 		return igual(this);
+	 * }
+	 */
 	
 	
 }
