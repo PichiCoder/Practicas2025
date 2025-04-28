c4ba0617bd0465f7740334b495f694cce387b4cc
Matias F. <86623987+PichiCoder@users.noreply.github.com>
2025-04-28 15:25:38 -0300

    Update ej9_ParcialArboles.java

--- eclipse_AyED_2025/src/tp3/ej9_ParcialArboles.java
+++ eclipse_AyED_2025/src/tp3/ej9_ParcialArboles.java
@@ -3,8 +3,9 @@ package tp3;
 import java.util.Iterator;
 
 public class ej9_ParcialArboles {
-	
-	private static boolean comprobarSiEsDeSeleccion (GeneralTree<Integer> arbol) {
+
+	//Incorrecto por lo mismo que el punto 8 de navidad
+	private static boolean comprobarSiEsDeSeleccion_VersionVieja (GeneralTree<Integer> arbol) {
 		boolean cumplo = true;
 		int minimo = 9999999;
 		
@@ -23,10 +24,32 @@ public class ej9_ParcialArboles {
 		
 		while (cumplo && it.hasNext()) {
 			GeneralTree<Integer> nodo = it.next();
-			cumplo = comprobarSiEsDeSeleccion(nodo);
+			cumplo = comprobarSiEsDeSeleccion_VersionVieja(nodo);
 		}
 
 		return cumplo;
+	
+	}
+	
+	private static int comprobarSiEsDeSeleccion (GeneralTree<Integer> arbol) {
+		int aux=0, minimo = 9999999;
+		
+		if (arbol.isLeaf())
+			return arbol.getData();
+		
+		Iterator<GeneralTree<Integer>> it = arbol.getChildren().iterator();
+		
+		while (aux != -1 && it.hasNext()) {
+			GeneralTree<Integer> nodo = it.next();
+			aux = comprobarSiEsDeSeleccion(nodo); // si aux devuelve -1, debo dejar de recorrer
+			if (aux < minimo)
+				minimo = aux;
+		}
+		
+		if (arbol.getData() != minimo)
+			return -1;
+
+		return minimo;
 	
 	}
 	
@@ -34,7 +57,8 @@ public class ej9_ParcialArboles {
 		
 		if (arbol != null && !arbol.isEmpty()) {
 			
-			return comprobarSiEsDeSeleccion(arbol);
+			if (comprobarSiEsDeSeleccion(arbol) != -1) 
+				return true;
 			
 		}
 		
